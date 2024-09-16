dea95a46fa328fb94d60c1dee114aa34c223f4b7
goobyd00 <143662996+goobyd00@users.noreply.github.com>
2024-09-16 01:33:51 -0400

    -

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('active');
     });
 });
+
+
+// var slideIndex = 1;
+
+//function openLightbox() {
+  //document.getElementById("lightbox").style.display = "block";
+  //showSlide(slideIndex);
+//}
+
+//function closeLightbox() {
+  //document.getElementById("lightbox").style.display = "none";
+//}
+
+//function changeSlide(n) {
+ // showSlide(slideIndex += n);
+//}
+
+//function currentSlide(n) {
+  //showSlide(slideIndex = n);
+//}
+
+//function showSlide(n) {
+  //var i;
+  //var slides = document.getElementsByClassName("slides");
+  //if (n > slides.length) {slideIndex = 1}
+  //if (n < 1) {slideIndex = slides.length}
+  //for (i = 0; i < slides.length; i++) {
+    //slides[i].style.display = "none";
+  //}
+  //slides[slideIndex-1].style.display = "block";
+//}
